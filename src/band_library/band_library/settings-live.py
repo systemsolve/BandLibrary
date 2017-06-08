7683d5f0bc84bddf6a1e0475f9258e3d4932f6b8
david <david@10.222.0.28>
2017-06-08 11:54:36 +1000

    fix static locations

--- src/band_library/band_library/settings-live.py
+++ src/band_library/band_library/settings-live.py
@@ -129,6 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 STATIC_URL = '/static/'
 
 # app settings

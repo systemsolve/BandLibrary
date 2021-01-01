2234c7c2d66d2d191dd3e2171462027d7043f1a4
david <david@10.222.0.28>
2021-01-01 11:13:06 +1100

    enhanced model to help research in addition to library searching

--- src/band_library/library/admin.py
+++ src/band_library/library/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
 from django.contrib.admin.widgets import AdminFileWidget
+from django.contrib.admin import SimpleListFilter
 from django.db import models
+from django.db.models import Count, Q
 from django.utils.safestring import mark_safe
 from django.forms.widgets import Textarea
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.urls import reverse
+
 from .models import Author
 from .models import Category
 from .models import Entry
 from .models import Instrument
 from .models import Program
 from .models import Country
+from .models import Source
+from .models import Publisher
+from .models import Genre
+from .models import Publication
+from .models import Condition
 import sys
 from .utilx  import error_log
 
@@ -25,14 +37,37 @@ class AdminImageWidget(AdminFileWidget):
                           (image_url, file_name))
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         return mark_safe(''.join(output))
+    
+
+
+class EmptyMediaFilter(admin.SimpleListFilter):
+    title = "Media State"
+    parameter_name = "media"
+    
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'Absent', ),
+            ('0', 'Present', ),
+        )
+
+    def queryset(self, request, queryset):
+        error_log("IMAGE FILTER: %s %s" % (str(request), str(self)))
+        if self.value() in ('0', '1'):
+            kwargs = { '{0}__isnull'.format(self.parameter_name) : self.value() == '1' }
+            if (self.value() == '1'):
+                return queryset.filter(Q(media__isnull=True)|Q(media__exact=''))
+            else:
+                return queryset.filter(media__isnull=False)
+        return queryset
 
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'callno', 'composer', 'arranger', 'image_present', 'duration', 'instrument')
-    list_filter = ('category', )
-    search_fields = ['title', 'composer__given', 'composer__surname', 'arranger__surname']
+    list_display = ('title', 'category', 'callno', 'composer', 'arranger', 'source', 'publisher', 'pubname', 'pubyear', 'image_present','instrument','digitised')
+    list_filter = ('category', 'genre','composer__country', 'source', EmptyMediaFilter)
+    search_fields = ['title', 'composer__given', 'composer__surname', 'arranger__surname','callno','comments']
     readonly_fields = ('image_link', 'image_present')
     save_on_top = True
-    fields = ('title', ('category', 'callno'), ('composer', 'arranger'), 'instrument', 'comments', 'media', 'image_link')
+    fields = ('title', ('category', 'callno'), 'genre','composer', 'arranger', ('publisher', 'pubyear', 'platecode'), ('pubname', 'pubissue'), ('source','provider'), 'instrument', ('comments','backpage'), ('media','condition','pagecount'), 'image_link', 'digitised')
+    autocomplete_fields = ['composer','arranger','provider']
     formfield_overrides = {
         models.TextField: {'widget': Textarea(
               attrs={
@@ -41,42 +76,154 @@ class EntryAdmin(admin.ModelAdmin):
               })
         },
     }
+    
+    """
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
 
+        qs = qs.annotate(_has_media=Count('compositions', distinct=True),
+            _arranger=Count('arrangements', distinct=True),
+            _provider=Count('donations', distinct=True))
+
+
+        #error_log("AUTHOR QS: %s" % qs.query)
+        return qs
+    """
+    
     def image_link(self, instance):
         error_log("IMAGE LINK: %s %s" % (str(instance.media), str(instance.id)))
         if instance.media:
             return mark_safe('<img width="300" src="%s" alt="%s" />' % ("/library/incipit/" + str(instance.id), "FRED"))
         else:
             return 'MISSING'
-        
-    
+
+
     image_link.short_description = "Image Link"
-    
+
     def image_present(self, instance):
-        
-        return not not instance.media            
-        
-    
+
+        return not not instance.media       # looks strange - ensures a boolean
+
+
     image_present.short_description = "Image"
     image_present.boolean = True
-    
-    
-    
+
+class ProgramAdmin(admin.ModelAdmin):
+    save_on_top = True
+    filter_horizontal = ['entry']
+
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('surname', 'given', 'country', 'bornyear', 'diedyear')
+    list_display = ('surname', 'given', 'arranger', 'composer', 'provider','country', 'bornyear', 'diedyear','realname')
     search_fields = ['surname', 'given']
+    list_filter = ('country',)
+    save_on_top = True
+    autocomplete_fields = ['realname']
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(
+              attrs={
+                  'rows': 5,
+                  'cols': 40
+              })
+        },
+    }
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        qs = qs.annotate(_composer=Count('compositions', distinct=True),
+            _arranger=Count('arrangements', distinct=True),
+            _provider=Count('donations', distinct=True))
+
+
+        #error_log("AUTHOR QS: %s" % qs.query)
+        return qs
+
+
+    def composer(self, instance):
+        return instance._composer
+
+
+    def arranger(self, instance):
+        return instance._arranger
+
+    def provider(self, instance):
+        return instance._provider
+
+    composer.admin_order_field = '_composer'
+    arranger.admin_order_field = '_arranger'
+    provider.admin_order_field = '_provider'
+
+
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'is_staff', 'last_login', 'is_superuser')
 
+
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = '<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return mark_safe(link)
+
+    object_link.admin_order_field = "object_repr"
+    object_link.short_description = "object"
+
+
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Program)
+admin.site.register(Program, ProgramAdmin)
 admin.site.register(Category)
 admin.site.register(Instrument)
 admin.site.register(Country)
+admin.site.register(Source)
+admin.site.register(Publisher)
+admin.site.register(Publication)
+admin.site.register(Condition)
+admin.site.register(Genre)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
--- src/band_library/library/models.py
+++ src/band_library/library/models.py
@@ -1,7 +1,7 @@
 
 
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, SET_NULL
 
 class LabelField(models.CharField):
 
@@ -18,20 +18,39 @@ class LabelField(models.CharField):
     def to_python(self, value):
         value = super(LabelField, self).to_python(value)
         return value.lstrip("0")
+    
+class Source(models.Model):
+    label = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return '%s' % self.label
 
 
 class Entry(models.Model):
     title = models.CharField(max_length=200)
     callno = LabelField(max_length=12, verbose_name="label", db_column="label")
+    pagecount = models.IntegerField(blank=True, null=True, verbose_name="Page count", help_text="for solo cornet")
     duration = models.CharField(max_length=8, default="0:0")
     composer = models.ForeignKey('Author', blank=True, null=True, related_name="compositions", on_delete=CASCADE)
     arranger = models.ForeignKey('Author', blank=True, null=True, related_name="arrangements", on_delete=CASCADE)
-    category = models.ForeignKey('Category', on_delete=CASCADE)
+    category = models.ForeignKey('Category', verbose_name='Shelving Category', on_delete=CASCADE)
+    genre = models.ForeignKey('Genre', verbose_name='Type/Genre of Work', blank=True, null=True, on_delete=SET_NULL)
+    publisher = models.ForeignKey('Publisher', blank=True, null=True, related_name="pubyears", on_delete=CASCADE)
+    pubyear = models.IntegerField(verbose_name='Year of edition', help_text="If year not known, use (e.g.) 1920 for decade", blank=True, null=True)
+    pubname = models.ForeignKey('Publication', verbose_name='Publication', blank=True, null=True, related_name="publications", on_delete=CASCADE)
+    pubissue = models.CharField(max_length=24,verbose_name='Vol/Issue', blank=True, null=True)
+    platecode = models.CharField(max_length=12, blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
+    backpage = models.TextField(verbose_name='Back page comments (e.g. ads)', blank=True, null=True)
     added = models.DateField(verbose_name='Date added', auto_now=True)
-    instrument = models.ForeignKey("Instrument", blank=True, null=True, on_delete=CASCADE)
+    instrument = models.ForeignKey("Instrument", blank=True, null=True, on_delete=SET_NULL)
     #media = models.CharField(max_length=256, blank=True, null=True)
     media = models.FileField(upload_to='', max_length=256, blank=True, null=True)
+    source = models.ForeignKey("Source", blank=True, null=True, related_name="+", on_delete=SET_NULL)
+    provider = models.ForeignKey('Author', blank=True, null=True, related_name="donations", on_delete=CASCADE)
+    digitised = models.BooleanField(default=False)
+    condition = models.ForeignKey('Condition', blank=True, null=True, related_name="+", on_delete=CASCADE)
 
     class Meta:
         #unique_together = ('callno', 'category')
@@ -43,6 +62,36 @@ class Entry(models.Model):
 
     def __str__(self):
         return '%s (%s,%s)' % (self.title, self.category.code, self.callno)
+    
+class Publication(models.Model):
+    name = models.CharField(max_length=128)    
+    publisher = models.ForeignKey('Publisher', blank=True, null=True, related_name="publications", on_delete=CASCADE)
+    comments = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('name', 'publisher')
+        ordering = ['name',]
+
+    def __str__(self):
+        if self.publisher:
+            return '%s (%s)' % (self.name, self.publisher)
+        else:
+            return '%s ()' % self.name
+    
+class Publisher(models.Model):
+    name = models.CharField(max_length=128)    
+    country = models.ForeignKey('Country', blank=True, null=True, related_name="publishers", on_delete=CASCADE)
+    comments = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('name', 'country')
+        ordering = ['name',]
+
+    def __str__(self):
+        if self.country:
+            return '%s (%s)' % (self.name, self.country)
+        else:
+            return '%s ()' % self.name
 
 class Author(models.Model):
     surname = models.CharField(max_length=32)
@@ -50,6 +99,7 @@ class Author(models.Model):
     bornyear = models.IntegerField(blank=True, null=True, verbose_name='Year of Birth')
     diedyear = models.IntegerField(blank=True, null=True, verbose_name='Year of Death')
     country = models.ForeignKey('Country', blank=True, null=True, related_name="authors", on_delete=CASCADE)
+    realname = models.ForeignKey('Author', blank=True, null=True, related_name="pennames", on_delete=CASCADE)
     comments = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -67,10 +117,20 @@ class Country(models.Model):
     isocode = models.CharField(max_length=3)
 
     class Meta:
+        verbose_name_plural = "Countries"
         ordering = ['name']
 
     def __str__(self):
-        return '%s (%s)' % (self.name, self.code)
+        return '%s (%s)' % (self.name, self.isocode)
+    
+class Condition(models.Model):
+    name = models.CharField(max_length=48)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return '%s' % (self.name)
 
 class Instrument(models.Model):
     name = models.CharField(max_length=48)
@@ -92,10 +152,21 @@ class Category(models.Model):
     def __str__(self):
         return '%s (%s)' % (self.label, self.code)
 
+class Genre(models.Model):
+    label = models.CharField(max_length=32)
+    comments = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Genres"
+        ordering = ['label']
+
+    def __str__(self):
+        return '%s' % (self.label)
+
 class Program(models.Model):
     included = models.DateField()
     comments = models.TextField(blank=True, null=True)
-    entry = models.ForeignKey('Entry', related_name="programmed", on_delete=CASCADE)
+    entry = models.ManyToManyField('Entry', related_name="programmed")
 
     def __str__(self):
         return '%s' % (self.included)

53b4ce4f01bf284e81824c031c6fe9b1e1431c25
david <david@10.222.0.28>
2024-03-18 08:29:09 +1100

    alternative makeimage, using ImageMagick "convert" instead of "pdftoppm"

--- src/band_library/library/utilx.py
+++ src/band_library/library/utilx.py
@@ -29,3 +29,22 @@ def makeimage(fname, res, prefix):
     modtime = os.path.getmtime(os.path.join(cachedir, target + ".jpg"))
     response['Last-Modified'] = datetime.datetime.utcfromtimestamp(modtime).strftime("%a, %d %b %y %H:%M:%S GMT")
     return response
+
+def makeimage2(fname, res, prefix):
+    target = prefix + fname
+    try:
+#        error_log("open %s" % os.path.join(cachedir, target + ".jpg"))
+        ffc = open(os.path.join(cachedir, target + ".jpg"), "rb")
+    except:
+        error_log("convert %s" % os.path.join(mediadir, fname))
+        command = "convert %s -jpeg \"%s\" \"%s\"" % (res, os.path.join(mediadir, fname), os.path.join(cachedir, target))
+        error_log("command %s" % command)
+        subprocess.call(command, shell=True)
+        ffc = open(os.path.join(cachedir, target + ".jpg"), "rb")
+
+    response = FileResponse(ffc, content_type="image/jpeg", charset='C')
+    response['Cache-Control'] = "public"
+
+    modtime = os.path.getmtime(os.path.join(cachedir, target + ".jpg"))
+    response['Last-Modified'] = datetime.datetime.utcfromtimestamp(modtime).strftime("%a, %d %b %y %H:%M:%S GMT")
+    return response
